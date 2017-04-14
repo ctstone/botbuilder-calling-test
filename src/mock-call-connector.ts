db4a0e4d2226599aae46e667fcb64b32cd34e0f2
Chris Stone <chstone@microsoft.com>
2017-04-14 16:11:12 -0400

    add request-from-file functions

--- src/mock-call-connector.ts
+++ src/mock-call-connector.ts
@@ -2,11 +2,19 @@ import async = require('async');
 import {
   IBotStorage, IBotStorageContext, IBotStorageData,
   ICallConnector, IEvent } from 'botbuilder-calling';
-// import { util } from './bot-util';
+import { BotCallRecorder, BotCallRecorderOptions, RewriteCallback } from './bot-call-recorder';
 
 export class MockCallConnector implements ICallConnector, IBotStorage {
+
   private handler: (event: IEvent, cb?: (err: Error) => void) => void;
   private responses: { [id: string]: (err: Error, repsonse?: any) => void; } = {};
+  private recorder: BotCallRecorder;
+
+  constructor(options?: BotCallRecorderOptions) {
+    if (options) {
+      this.recorder = new BotCallRecorder(options);
+    }
+  }
 
   onEvent(handler: (event: IEvent, callback?: (err: Error) => void) => void): void {
     this.handler = handler;
@@ -47,14 +55,18 @@ export class MockCallConnector implements ICallConnector, IBotStorage {
     async.mapSeries(events, (event, next) => this.request(event, next), response);
   }
 
-  // requestFromFile(filepath:string, response: (err: Error, response?: any) => void) {
-  //   util.readEventLocal(filepath, (err, event) => {
-  //     if (err) return response(err);
-  //     this.request(event, response);
-  //   });
-  // }
+  requestFromFile(file: string, response: (err: Error, response?: any) => void) {
+    if (!this.recorder) {
+      throw new Error('Must supply BotCallRecorderOptions to use requestFromFile');
+    }
 
-  // requestFromFiles(files: string[], response: (err: Error, response?: any[]) => void): void {
-  //   async.mapSeries(files, (filepath, next) => this.requestFromFile(filepath, next), response);
-  // }
+    async.waterfall([
+      (next: ErrorCallback<any>) => this.recorder.read(file, next),
+      (event: any, next: ErrorCallback<any>) => this.request(event, next),
+    ], response);
+  }
+
+  requestFromFiles(files: string[], response: (err: Error, response?: any[]) => void): void {
+    async.mapSeries(files, (filepath, next) => this.requestFromFile(filepath, next), response);
+  }
 }
